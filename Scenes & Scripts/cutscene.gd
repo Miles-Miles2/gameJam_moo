a3beb1c188077846090c0fb7aa1080058d3a635b
Filip K <filip92027@gmail.com>
2025-06-01 18:47:27 -0700

    cutscenes

---
+++ Scenes & Scripts/cutscene.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+@export var ending = false
+
+func _ready() -> void:
+	$Cutscene.play("cutscene")
+	if (ending == false) :
+		await get_tree().create_timer(36.5).timeout
+		$ColorRect/AnimationPlayer.play("close")
+		await get_tree().create_timer(0.6).timeout
+		get_tree().change_scene_to_file("res://Scenes & Scripts/haybale_test.tscn")
+	else :
+		pass
+	
+
+
+func _on_button_2_pressed() -> void:
+	$ColorRect/AnimationPlayer.play("close")
+	await get_tree().create_timer(0.6).timeout
+	get_tree().quit()
+func _on_button_pressed() -> void:
+	$ColorRect/AnimationPlayer.play("close")
+	await get_tree().create_timer(0.6).timeout
+	get_tree().change_scene_to_file("res://Scenes & Scripts/haybale_test.tscn")
